742d633eea5dd8c2ef89ba52ad90f420652c9aa8
devops <duttashaan102@gmail.com>
2020-09-01 04:17:07 +0530

    Initial development

---
+++ main.py
@@ -0,0 +1,31 @@
+from os import system as command
+from platform import system as sys
+
+def clr():
+    if sys() == "Linux":
+        command("clear")
+
+    if sys() == "Darwin":
+        command("clear")
+    
+    if sys() == "Windows":
+        command("cls")
+
+    
+def pause(st = ""):
+    input(st)
+
+def main():
+    while True:
+        pause("""
+        Welcome to the Python Learning Program. This program will teach you about python from the basics
+        of python(Only as much as i know about python, which is not much). 
+
+        Expect this project to grow with time as more and more developers find their interests in it
+
+        ***Please note that this program does not provide a formal degree or something that you can add to
+        your job resume or something like that. This is only a fun project with great motives. All the content
+        used here is either self made or used under the conditions of "fair-use".***
+        
+        Hope you will have fun! Please press enter to continue!
+        """)
